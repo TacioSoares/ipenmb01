991814e60da96f23453df12c1324d04ea075b135
Tácio Soares <taciofsoares@gmail.com>
2022-10-14 16:28:38 -0300

    evolução

---
+++ eletricidade/logica/sistemaseletricos.js
@@ -0,0 +1,150 @@
+var res1 = document.querySelector('#res1')
+var res2 = document.querySelector('#res2')
+var res3 = document.querySelector('#res3')
+var res4 = document.querySelector('#res4')
+var res5 = document.querySelector('#res5')
+var res6 = document.querySelector('#res6')
+var res7 = document.querySelector('#res7')
+var res8a = document.querySelector('#res8a')
+var res8b = document.querySelector('#res8b')
+var res9 = document.querySelector('#res9')
+var res10 = document.querySelector('#res10')
+var res11 = document.querySelector('#res11')
+var res12 = document.querySelector('#res12')
+var res13 = document.querySelector('#res13')
+var res14 = document.querySelector('#res14')
+var res15 = document.querySelector('#res15')
+var res16a = document.querySelector('#res16a')
+var res16b = document.querySelector('#res16b')
+var res17 = document.querySelector('#res17')
+var res18 = document.querySelector('#res18')
+var res19 = document.querySelector('#res19')
+var res20 = document.querySelector('#res20')
+var res21 = document.querySelector('#res21')
+var res22 = document.querySelector('#res22')
+var res23 = document.querySelector('#res23')
+var res24 = document.querySelector('#res24')
+var res25 = document.querySelector('#res25')
+var res26 = document.querySelector('#res26')
+
+
+
+function resposta(n) {
+    if (n == 1) {
+        res1.style.display = 'block'
+    } else if (n == 2) {
+        res2.style.display = 'block'
+    } else if (n == 3) {
+        res3.style.display = 'block'
+    } else if (n == 4) {
+        res4.style.display = 'block'
+    } else if (n == 5) {
+        res5.style.display = 'block'
+    } else if (n == 6) {
+        res6.style.display = 'block'
+    } else if (n == 7) {
+        res7.style.display = 'block'
+    } else if (n == 8) {
+        res8a.style.display = 'block'
+    } else if (n == 88) {
+        res8b.style.display = 'block'
+    } else if (n == 9) {
+        res9.style.display = 'block'
+    } else if (n == 10) {
+        res10.style.display = 'block'
+    } else if (n == 11) {
+        res11.style.display = 'block'
+    } else if (n == 12) {
+        res12.style.display = 'block'
+    } else if (n == 13) {
+        res13.style.display = 'block'
+    } else if (n == 14) {
+        res14.style.display = 'block'
+    } else if (n == 15) {
+        res15.style.display = 'block'
+    } else if (n == 16) {
+        res16a.style.display = 'block'
+    } else if (n == 1616) {
+        res16b.style.display = 'block'
+    } else if (n == 17) {
+        res17.style.display = 'block'
+    } else if (n == 18) {
+        res18.style.display = 'block'
+    } else if (n == 19) {
+        res19.style.display = 'block'
+    } else if (n == 20) {
+        res20.style.display = 'block'
+    } else if (n == 21) {
+        res21.style.display = 'block'
+    } else if (n == 22) {
+        res22.style.display = 'block'
+    } else if (n == 23) {
+        res23.style.display = 'block'
+    } else if (n == 24) {
+        res24.style.display = 'block'
+    } else if (n == 25) {
+        res25.style.display = 'block'
+    } else {
+        res26.style.display = 'block'
+    }
+}
+
+function apagar(n) {
+if (n == 1) {
+    res1.style.display = 'none'
+} else if (n == 2) {
+    res2.style.display = 'none'
+} else if (n == 3) {
+    res3.style.display = 'none'
+} else if (n == 4) {
+    res4.style.display = 'none'
+} else if (n == 5) {
+    res5.style.display = 'none'
+} else if (n == 6) {
+    res6.style.display = 'none'
+} else if (n == 7) {
+    res7.style.display = 'none'
+} else if (n == 8) {
+    res8a.style.display = 'none'
+} else if (n == 88) {
+    res8b.style.display = 'none'
+} else if (n == 9) {
+    res9.style.display = 'none'
+} else if (n == 10) {
+    res10.style.display = 'none'
+} else if (n == 11) {
+    res11.style.display = 'none'
+} else if (n == 12) {
+    res12.style.display = 'none'
+} else if (n == 13) {
+    res13.style.display = 'none'
+} else if (n == 14) {
+    res14.style.display = 'none'
+} else if (n == 15) {
+    res15.style.display = 'none'
+} else if (n == 16) {
+    res16a.style.display = 'none'
+} else if (n == 1616) {
+    res16b.style.display = 'none'
+} else if (n == 17) {
+    res17.style.display = 'none'
+} else if (n == 18) {
+    res18.style.display = 'none'
+} else if (n == 19) {
+    res19.style.display = 'none'
+} else if (n == 20) {
+    res20.style.display = 'none'
+} else if (n == 21) {
+    res21.style.display = 'none'
+} else if (n == 22) {
+    res22.style.display = 'none'
+} else if (n == 23) {
+    res23.style.display = 'none'
+} else if (n == 24) {
+    res24.style.display = 'none'
+} else if (n == 25) {
+    res25.style.display = 'none'
+} else {
+    res26.style.display = 'none'
+}
+}
