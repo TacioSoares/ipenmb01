222749b752fd7103089691c4dbb43df711e3d8c1
Tácio Soares <taciofsoares@gmail.com>
2022-12-21 14:04:27 -0300

    fim do modelo

---
+++ teste/logica/mecanismos.js
@@ -0,0 +1,88 @@
+// funções chamadas nas questões
+
+function respostaCorreta(n) {
+    var feedback = document.querySelector(`#correção${n}`)
+    feedback.style.color = 'darkgreen'
+    feedback.innerHTML = 'Correto!'
+}
+
+function respostaMeioCerta(n) {
+    var feedback = document.querySelector(`#correção${n}`)
+    feedback.style.color = 'lightgreen'
+    feedback.innerHTML = 'Meio certo!'
+}
+
+function respostaIncorreta(n) {
+    var feedback = document.querySelector(`#correção${n}`)
+    feedback.style.color = 'darkred'
+    feedback.innerHTML = 'Incorreto!'
+}
+
+function mostrarResposta(n) {
+    var respotaCerta = document.querySelector(`#resp${n}`)
+    respotaCerta.style.display = 'block'
+}
+
+function mostrarAlternativaCerta(n) {
+    var correta = document.querySelector(`#m0${n}`)
+    correta.style.backgroundColor = 'lightgreen'
+}    
+
+function contarPonto(input) {
+    ponto = 0
+    input.forEach(caixa => {
+        if(caixa.checked) {
+            if((correta.indexOf(caixa.value)) != -1){
+                ponto += 1
+            } else {
+                ponto -= 1
+            }
+        }
+    });
+    return (ponto)
+}
+
+function validaVouF(ponto, correta, rCerta) {
+    if (ponto == correta.length) {
+        respostaCorreta(4)
+    } else {
+        respostaIncorreta(4)
+    }
+    rCerta.forEach(certa => {
+        certa.style.backgroundColor = 'lightgreen'
+    })
+}
+
+function verificarResposta(input, item1, item2, item3) {
+    var resp1 = new RegExp(item1,'gi')
+    var resp2 = new RegExp(item2,'gi')
+    var resp3 = new RegExp(item3,'gi')
+    var ponto = 0
+    if (resp1.test(input) && resp2.test(input) && resp3.test(input)) {
+        ponto = 3
+        console.log(ponto)
+    } else {
+        ponto = 0
+        console.log(ponto)
+    }
+    return ponto
+}
+
+function embrulha(questão) {
+    var c = questão.length - 1
+    for (c;c>0;c--) {
+        var i = Math.floor(Math.random() * c+1);
+        [questão[c].innerHTML, questão[i-1].innerHTML] = [questão[i-1].innerHTML, questão[c].innerHTML];
+    }
+    return questão
+}
+
+function mostrarErro(errada) {
+    errada.style.backgroundColor = 'red'
+}
+
+function limpaTudo(todas) {
+    todas.forEach(alternativa => {
+        alternativa.style.backgroundColor = 'white'
+    });
+}
