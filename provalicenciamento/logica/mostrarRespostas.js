eb6a7cdc6f991c3a733e11393b612a4b477d3e97
Tácio Soares <taciofsoares@gmail.com>
2023-02-03 14:08:47 -0300

    22020

--- provalicenciamento/logica/mostrarRespostas.js
+++ provalicenciamento/logica/mostrarRespostas.js
@@ -3,7 +3,6 @@ var botaoDeResposta = document.querySelectorAll('.chamaResposta')
 var campoResposta = pegaCampoResposta()
 for (let i = 0; i < botaoDeResposta.length; i++) {
     botaoDeResposta[i].addEventListener('click', function(){
-       // console.log(this.parentNode.nextElementSibling)
         
         if (botaoDeResposta[i].value == 'Resposta') {
             this.parentNode.nextElementSibling.style.display = 'block'
