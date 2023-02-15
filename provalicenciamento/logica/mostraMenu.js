e82b112b263f4dc57fdf688920b697b7f1397194
Tácio Soares <taciofsoares@gmail.com>
2023-02-15 18:42:24 -0300

    Ok

--- provalicenciamento/logica/mostraMenu.js
+++ provalicenciamento/logica/mostraMenu.js
@@ -3,11 +3,22 @@ var checkBox = document.querySelectorAll('.filtro')
 
 botaoMenu.addEventListener("click", function() {
     var opcaoMenu = document.querySelector('#janelaMenu')
-    
+    var listaMenu = document.querySelectorAll('.opcaoMenu')
     if (opcaoMenu.style.display == 'flex') {
-        opcaoMenu.style.display = 'none'
+        listaMenu.forEach(opcao => {
+            opcao.style.opacity = '0'
+        })
+        setTimeout(() => {
+            opcaoMenu.style.display = 'none'
+        },1200)
     } else {
         opcaoMenu.style.display = 'flex'
+        setTimeout(() => {
+            listaMenu.forEach(opcao => {
+                opcao.style.opacity = '100'
+            })
+        },300)
+        
     }
 })
 
@@ -19,12 +30,18 @@ checkBox.forEach(check => {
             if(elemento.checked == false){
                 var questoes = document.querySelectorAll(`.questãoSecao${elemento.name}`)
                 questoes.forEach(questao => {
-                    questao.style.display = 'none'
+                    questao.style.opacity = '0'
+                    setTimeout(() => {
+                        questao.style.display = 'none'
+                    }, 1500);
                 })
             } else {
                 var questoes = document.querySelectorAll(`.questãoSecao${elemento.name}`)
                 questoes.forEach(questao => {
                     questao.style.display = 'block'
+                    setTimeout(() => {
+                        questao.style.opacity = '100'
+                    }, 300);
                 })
             }
         })
