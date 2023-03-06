5b9c7c4f8b9f4811b76ac54c3d2a9f54969c0949
Tácio Soares <taciofsoares@gmail.com>
2023-03-06 11:43:08 -0300

    Alterações na animação

--- provalicenciamento/logica/mostrarRespostas.js
+++ provalicenciamento/logica/mostrarRespostas.js
@@ -5,11 +5,15 @@ for (let i = 0; i < botaoDeResposta.length; i++) {
     botaoDeResposta[i].addEventListener('click', function(){
         
         if (botaoDeResposta[i].value == 'Resposta') {
+            this.parentNode.nextElementSibling.style.animation = 'mostrar2 1s ease-in-out forwards'
             this.parentNode.nextElementSibling.style.display = 'block'
             botaoDeResposta[i].value = 'Ocultar'
         } else {
-            this.parentNode.nextElementSibling.style.display = 'none'
-            botaoDeResposta[i].value = 'Resposta'
+            this.parentNode.nextElementSibling.style.animation = 'ocultar 1s ease-in-out forwards'
+            setTimeout(() => {
+                this.parentNode.nextElementSibling.style.display = 'none'
+                botaoDeResposta[i].value = 'Resposta'
+            }, 900);
         }    
     })    
 }
