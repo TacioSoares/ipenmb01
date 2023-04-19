67eca8201b15aa0589c185897c942bcde937cad0
Tácio Soares <taciofsoares@gmail.com>
2023-04-19 08:03:15 -0300

    fd

--- provalicenciamento/logica/declaracao2.js
+++ provalicenciamento/logica/declaracao2.js
@@ -686,7 +686,7 @@ const secaoG = [
         pergunta: `Cite exemplos de condições de alerta`,
         resposta: `Linha de Alimentação do Tanque do Moderador - 2,5 10<sup>-1</sup> mGy/h <br><br>
         Tanque de Decaimento - 2,5 10<sup>-2</sup> mGy/h <br><br>
-        Tanque do Moderador - (Plataforma) - 2,5 101 mGy/h <br><br>
+        Tanque do Moderador - (Plataforma) - 2,5 10<sup>1</sup> mGy/h <br><br>
         Linha de Exaustão (Chaminé) - 10<sup>3</sup> cpm
         .
         `
